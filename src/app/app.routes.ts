eaca556875df2c89eb84a485979983ff7ee3a65c
Thando <thandwem045@gmail.com>
2026-01-28 20:59:01 +0200

    Added routing to the profile and the home page of the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainPg } from './main-pg/main-pg';
+import { ProfilePg } from './profile-pg/profile-pg';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: MainPg, title: "Home"},
+    { path: "profile", component: ProfilePg, title: "Profile"}
+];
