91de00e76bf023344e500a616dfa14450ef8fa50
Thando <thandwem045@gmail.com>
2026-01-28 21:36:11 +0200

    Created 2 more components and added routing

--- src/app/main-pg/main-pg.ts
+++ src/app/main-pg/main-pg.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-main-pg',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-pg.html',
   styleUrl: './main-pg.css',
 })
